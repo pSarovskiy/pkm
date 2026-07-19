267ecf366403fb197b6a2ca95856959e544434e2
pSarovskiy <sarovskiy.pavel@gmail.com>
2026-07-19 14:05:08 +0300

    Delete config directory

--- config/celery.py
+++
@@ -1,9 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
-
-app = Celery("wp_django")
-app.config_from_object("django.conf:settings", namespace="CELERY")
-app.conf.broker_connection_retry_on_startup = True
-app.autodiscover_tasks()
