600ec1935c80aea54c18eff0de95c36434f63bf5
pSarovskiy <sarovskiy.pavel@gmail.com>
2026-07-19 14:04:56 +0300

    Delete apps directory

--- apps/content/api_urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-
-from .api_views import PostViewSet
-
-router = DefaultRouter()
-router.register("posts", PostViewSet, basename="api-post")
-
-urlpatterns = router.urls
--- apps/nlp/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class NlpConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.nlp"
-    verbose_name = "NLP-анализ"
